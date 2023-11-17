add88bbc548945256de7543c82e5ed146cba0958
Vanessa <v.mendoza1@gmail.com>
2023-11-17 11:29:19 -0500

    1. Created ReservationServiceImpl class and methods

--- vacation-home-demo/src/main/java/com/example/vacationhomedemo/repository/ReservationRepository.java
+++ vacation-home-demo/src/main/java/com/example/vacationhomedemo/repository/ReservationRepository.java
@@ -3,7 +3,10 @@ package com.example.vacationhomedemo.repository;
 import com.example.vacationhomedemo.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository <Reservation, Integer> {
 
 
+    List<Reservation> findAllByOrderByLastNameAsc();
 }
