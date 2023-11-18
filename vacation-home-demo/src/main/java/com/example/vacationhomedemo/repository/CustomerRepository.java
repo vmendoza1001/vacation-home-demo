ed89255e8753be886006a5f82055ac480b996a40
Vanessa <v.mendoza1@gmail.com>
2023-11-18 16:02:14 -0500

    1. Created CustomerServiceImpl class and methods

--- vacation-home-demo/src/main/java/com/example/vacationhomedemo/repository/CustomerRepository.java
+++ vacation-home-demo/src/main/java/com/example/vacationhomedemo/repository/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.example.vacationhomedemo.repository;
 import com.example.vacationhomedemo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository <Customer, Integer> {
 
+    List<Customer> findAllByOrderByLastNameAsc();
 }
