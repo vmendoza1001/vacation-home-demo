4c909ecc74af9c2a49efe85f9eb1037cb777a814
Vanessa <v.mendoza1@gmail.com>
2023-11-18 15:41:15 -0500

    1. Created EmployeeServiceImpl class and methods

--- vacation-home-demo/src/main/java/com/example/vacationhomedemo/repository/EmployeeRepository.java
+++ vacation-home-demo/src/main/java/com/example/vacationhomedemo/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.example.vacationhomedemo.repository;
 import com.example.vacationhomedemo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
 
+    List<Employee> findAllByOrderByLastNameAsc();
 }
