118ab9e00197e41b035ca1d8f52f504688e2215f
Vanessa <v.mendoza1@gmail.com>
2023-11-19 12:31:38 -0500

    Created Security.UserService

--- vacation-home-demo/src/main/java/com/example/vacationhomedemo/security/service/UserService.java
+++ vacation-home-demo/src/main/java/com/example/vacationhomedemo/security/service/UserService.java
@@ -1,4 +1,12 @@
 package com.example.vacationhomedemo.security.service;
 
-public class UserService {
+import com.example.vacationhomedemo.security.user.WebUser;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    public User findByUserName(String userName);
+
+    void save(WebUser webUser);
 }
