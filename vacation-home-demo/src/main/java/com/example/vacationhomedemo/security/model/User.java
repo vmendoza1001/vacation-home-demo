1e60eebc051b79dcf0d38efaa2787d017b2a2c03
Vanessa <v.mendoza1@gmail.com>
2023-11-19 14:12:23 -0500

    Created WebSecurityConfig class, WebUser class, added validation dependency to pom.xml, added email field to User class

--- vacation-home-demo/src/main/java/com/example/vacationhomedemo/security/model/User.java
+++ vacation-home-demo/src/main/java/com/example/vacationhomedemo/security/model/User.java
@@ -22,6 +22,9 @@ public class User {
     @Column(name = "enabled")
     private boolean enabled;
 
+    @Column(name = "email")
+    private String email;
+
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "users_roles",
         joinColumns = @JoinColumn(name = "user_id"),
@@ -76,6 +79,14 @@ public class User {
         this.enabled = enabled;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Collection<Role> getRoles() {
         return roles;
     }
@@ -91,6 +102,7 @@ public class User {
                 ", userName='" + userName + '\'' +
                 ", password='" + password + '\'' +
                 ", enabled=" + enabled +
+                ", email='" + email + '\'' +
                 ", roles=" + roles +
                 '}';
     }
--- vacation-home-demo/src/main/java/com/example/vacationhomedemo/security/securityconfig/WebSecurityConfig.java
+++ vacation-home-demo/src/main/java/com/example/vacationhomedemo/security/securityconfig/WebSecurityConfig.java
@@ -1,4 +1,56 @@
 package com.example.vacationhomedemo.security.securityconfig;
 
+
+import com.example.vacationhomedemo.security.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class WebSecurityConfig {
+
+    // bcrypt bean definition
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // authenticateProvider bean definition
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService) {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService); // set the custom user details service
+        auth.setPasswordEncoder(passwordEncoder()); // set the password encoder - bcrypt
+        return auth;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                //.requestMatchers("/")
+                                .requestMatchers("/register/**").permitAll()
+                                .requestMatchers("/customers/**").hasAnyRole("CUSTOMER", "EMPLOYEE", "ADMINISTRATOR")
+                                .requestMatchers("/reservation-list").hasAnyRole("EMPLOYEE", "ADMINISTRATOR")
+                                .requestMatchers("/employees/**").hasRole("ADMINISTRATOR")
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied")
+                );
+
+        return http.build();
+    }
 }
